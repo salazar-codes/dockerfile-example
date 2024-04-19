ac9655e5c731da0b5ccee3bac0ae4cf6f8bca2e7
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-04-19 14:55:26 -0500

    feat: refactor function

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const cron = require('node-cron');
+const { syncDB } = require('./tasks/sync-db');
 
 console.log("Inicio de la aplicación!!")
-
-let times = 0;
-
-cron.schedule('1-59/5 * * * * * *', () => {
-    times++;
-    console.log('Tick cada multiplo de 5: ',times);
-});
+cron.schedule('1-59/5 * * * * * *', syncDB);
